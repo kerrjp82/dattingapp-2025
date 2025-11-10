0528b3fa98688435b16348d25528962f9d37080a
James <kerrjp@hotmail.com>
2025-11-10 13:16:28 +0000

    End of section 7

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("auth")]
+        public IActionResult GetAuth()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("not-found")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("server-error")]
+        public IActionResult GetServerError()
+        {
+            throw new Exception("this is a server error!");
+        }
+        
+        [HttpGet("bad-request")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("this was a bad request!");
+        }
+    }
+}
